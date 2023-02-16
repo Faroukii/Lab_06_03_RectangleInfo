9dc87c677098615008d385ee353d96f597185c7b
Faroukii <122316189+Faroukii@users.noreply.github.com>
2023-02-15 19:07:19 -0500

    rectangle

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        double length = 0;
+        double width = 0;
+        String trash = "";
+        System.out.println("Please enter the length: ");
+
+        if (in.hasNextInt())
+        {
+            length = in.nextInt();
+            System.out.println("Please enter the width: ");
+            if (in.hasNextInt())
+            {
+                width = in.nextInt();
+                System.out.println("The area of your rectangle is:" + (length * width));
+                System.out.println("The diagonal of your rectangle is: " + ((length * length) + (width *width)));
+
+            }
+            else
+            {
+                System.out.println("Please enter correct value: " + trash);
+            }
+        }
+
+        else
+        {
+            trash = in.nextLine();
+            System.out.println("Please enter correct value: " + trash);
+        }
+
     }
 }
